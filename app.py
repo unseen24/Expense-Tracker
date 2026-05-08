dc11516a224e24f2a3135fcc65017a4dd23041f0
unseen24 <endicrenz24@gmail.com>
2026-05-08 16:58:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,55 @@ import json
 import os
 import datetime
 
-tasks = {
-        "id": 0,
-        "description": "",
-        "amount": "",
-        "createdAt": "",
-        "updatedAt": ""
-    }
-
 #Users can add an expense with a description and amount.
 def add_expense(description, amount):
-    print(f'{description} {amount}')
+
+    date = datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")
+
+    try:
+        with open('expenses.json') as f:
+            file = json.load(f)
+
+            if any(expense['description'] == description for expense in file['expenses']):
+                print('This expense already exist')
+
+            else:
+                
+                last_id = file['expenses'][-1].get('id') if file['expenses'] else 0
+                new_id = last_id + 1
+
+                new_expense = {
+                    "id": new_id,
+                    "description": description,
+                    "amount": amount,
+                    "date": date,
+                }
+
+                file['expenses'].append(new_expense)
+
+        with open('expenses.json', 'w') as f:
+            json.dump(file, f, indent=4)
+
+        print("Expense added successfully. ID:", file["expenses"][-1].get('id'))
+
+    except FileNotFoundError: 
+        with open('expenses.json', 'w') as f:
+
+            file_data = {
+                        'expenses': [
+                            {
+                            "id": 1,
+                            "description": description,
+                            "amount": amount,
+                            "date": date
+                        }
+                    ]
+                }
+            
+            json.dump(file_data, f, indent=4)
+
+            print("Expense added successfully. ID:", file["expenses"][-1].get('id'))
+
 
 #Users can update an expense.
 def update_expense(id):
